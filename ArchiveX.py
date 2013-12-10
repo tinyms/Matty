3d642b39d6aeb5c74316d71c4c393773c006c6c7
tinyms <i@tinyms.com>
2013-12-10 18:22:21 +0800

    1.0b

--- ArchiveX.py
+++ ArchiveX.py
@@ -2,8 +2,6 @@ __author__ = 'tinyms'
 
 import os
 import sys
-import base64
-import uuid
 import webbrowser
 
 from tornado.ioloop import IOLoop
@@ -20,7 +18,8 @@ ws_settings = dict()
 ws_settings["debug"] = True
 ws_settings["static_path"] = os.path.join(os.getcwd(), "static")
 ws_settings["template_path"] = os.path.join(os.getcwd(), "templates")
-ws_settings["cookie_secret"] = "askgpoi45345j2l34j6lkjasljdfpajsfd"#(base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes)).decode("utf-8")
+#(base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes)).decode("utf-8")
+ws_settings["cookie_secret"] = "askgpoi45345j2l34j6lkjasljdfpajsfd"
 
 ws_settings["ui_modules"] = dict()
 for key in ObjectPool.ui_mapping:
@@ -30,13 +29,14 @@ port = 80
 web_configs = Plugin.get(IWebConfig)
 if web_configs:
     for web_config in web_configs:
-        if hasattr(web_config,"debug"):
+        if hasattr(web_config, "debug"):
             ws_settings["debug"] = web_config.debug()
+            ObjectPool.mode_dev = ws_settings["debug"]
         if hasattr(web_config, "server_port"):
             port = web_config.server_port()
         if hasattr(web_config, "settings"):
             web_config.settings(ws_settings)
-        #Only one in application
+            #Only one in application
         if hasattr(web_config, "database_driver"):
             SessionFactory.__engine__ = web_config.database_driver()
             SessionFactory.create_tables()
@@ -45,7 +45,7 @@ if web_configs:
 #compress js and css file to one
 Utils.combine_text_files(os.path.join(os.getcwd(), "static/jslib/"), "tinyms.common")
 for urls in ObjectPool.url_patterns:
-    print("Url Mapping:",urls)
+    print("Url Mapping:", urls)
 app = Application(ObjectPool.url_patterns, **ws_settings)
 
 if __name__ == "__main__":
